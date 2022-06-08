d479a2c6e56dfed5d597cef23ac3ac1e72eac0bb
Benjamin <sparty182020@gmail.com>
2022-06-07 20:52:41 -0400

    updates

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const commands = require('mongodb/lib/core/connection/commands');
-
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
@@ -30,7 +28,7 @@ module.exports = (app) => {
         });
         return context.octokit.issues.createComment(issueComment);
     });
-    app.on('issue_comment',
+    app.on('issue_comment.created',
         async (context) => {
             //commands
             const commenter = context.payload.comment.user.login;
@@ -52,31 +50,109 @@ module.exports = (app) => {
                     command[command.length] = data.slice(4)
                 })
             }
-            command.forEach((data) => {
-                switch (data.toLowerCase()) {
-                    case 'addl.bug':
-                        context.octokit.issues.addLabels(context.issue({
-                            labels: ['bug']
-                        }))
-                        break
-                    case 'addl.feature':
-                        context.octokit.issues.addLabels(context.issue({
-                            labels: ['enhancement']
-                        }))
-                        break
-                    case 'close':
-                        context.octokit.issues.update(context.issue({
-                            state: 'closed'
-                        }))
-                        break
-                    case 'lock':
-                        context.octokit.issues.lock(context.issue({
-                            lock_reason: 'resolved'
-                        }))
-                        break
-                    default:
-                        break
-                }
+            Promise.resolve().then(_ => {
+                command.forEach((data) => {
+                    if (data.startsWith('addl.')) {
+                        data = data.slice(5)
+                    }
+                    switch (data.toLowerCase()) {
+                        case 'bug':
+                            context.octokit.issues.addLabels(context.issue({
+                                labels: ['bug']
+                            }))
+                            break
+                        case 'feature':
+                            context.octokit.issues.addLabels(context.issue({
+                                labels: ['enhancement']
+                            }))
+                            break
+                        case 'close':
+                            context.octokit.issues.update(context.issue({
+                                state: 'closed'
+                            }))
+                            break
+                        case 'lock':
+                            context.octokit.issues.lock(context.issue({
+                                lock_reason: 'resolved'
+                            }))
+                            break
+                        default:
+                            break
+                    }
+                })
+            })
+            .then(_ => {
+                context.octokit.issues.deleteComment(
+                    {
+                        owner: context.payload.repository.owner.login,
+                        repo: context.payload.repository.name,
+                        comment_id: context.payload.comment.id
+                    }
+                )
+            })
+        }
+    )
+    app.on('issue_comment.edited',
+        async (context) => {
+            //commands
+            const commenter = context.payload.comment.user.login;
+            const comment = context.payload.comment.body;
+            if (commenter !== 'sparty182020' /* Repo Owner */) return
+            if (!comment.includes('bot.')) return
+            /* Valid Commands:
+            * bot.addl.bug  => adds bug label
+            * bot.addl.feature => adds enhancement label
+            * bot.close => closes issue
+            * bot.lock => locks issue
+            */
+            let command = []
+            let multiline = (comment.split('\n').length > 1)
+            if (!multiline) {
+                command = [comment.slice(4)]
+            } else {
+                comment.split('\n').forEach((data) => {
+                    command[command.length] = data.slice(4)
+                })
+            }
+            Promise.resolve().then(_ => {
+                command.forEach((data) => {
+                    if (data.startsWith('addl.')) {
+                        data = data.slice(5)
+                    }
+                    switch (data.toLowerCase()) {
+                        case 'bug':
+                            context.octokit.issues.addLabels(context.issue({
+                                labels: ['bug']
+                            }))
+                            break
+                        case 'feature':
+                            context.octokit.issues.addLabels(context.issue({
+                                labels: ['enhancement']
+                            }))
+                            break
+                        case 'close':
+                            context.octokit.issues.update(context.issue({
+                                state: 'closed'
+                            }))
+                            break
+                        case 'lock':
+                            context.octokit.issues.lock(context.issue({
+                                lock_reason: 'resolved'
+                            }))
+                            break
+                        default:
+                            break
+                    }
+                })
+            })
+            .then(_ => {
+                context.octokit.issues.deleteComment(
+                    {
+                        owner: context.payload.repository.owner.login,
+                        repo: context.payload.repository.name,
+                        comment_id: context.payload.comment.id
+                    }
+                )
             })
         }
     )
